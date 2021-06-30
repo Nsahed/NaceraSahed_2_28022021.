94e3ed1669920bc2330f426eb5d44df84ca75a17
NSG <nacerasahed@hotmail.com>
2021-06-30 15:23:15 +0200

    commit project 2 oc

---
+++ PCHALET_01_code/htdocs/wp-config.php
@@ -0,0 +1,35 @@
+<?php
+
+
+define('AUTH_SALT', 'CGJ]@7J){R(IF`7?`3 Q|4A^dm;M@T~O>4 _apP0/9Us!gs.|V#V~:"/5s-&K8DK');
+
+define('DB_NAME', '1612946372_chaletsetcaviar_nacerasahed_dev');
+
+define('DB_COLLATE', 'utf8mb4_general_ci');
+
+define('AUTH_KEY', '@hV|Q*CrQHfA|;;i[KGf6I"yDPdF=M1]kKO"mrl*`j,,aU[%}TSj+Zg4oj*fd&FW');
+
+define('DB_PASSWORD', 'FxL3nxpqdsMmrvhP9JWFYBEFfVmwl0TY');
+
+define('NONCE_SALT', 'rHDpPE7Wk*WIJ"@gyeH[VJp0+)hBW"p4|xU?"$k0B,iN_}"5Qr$Aww28PDWk%%&d');
+
+define('NONCE_KEY', '/kn[{)Yx=71V~Yf](9]BofiS5n!>Fa_`,[4Tw.ek3.ryI<l39+y}[Jr)BxdpMF5P');
+
+define('LOGGED_IN_SALT', '[3KNu+|.FiU]<p{#St-y{}6_3AWeQX?MX(vx>Dru?^~8Z^l11}}^dtmbF$.U<[k7');
+
+define('LOGGED_IN_KEY', '"B2WNMbn;`!l>[>}.Te10nkYPBC=u612@Q*wr>"]xiBaZ6->"b$s~w-I1 VnATF!');
+
+define('DB_USER', '1612946372_chaletsetcaviar_nacer');
+
+define('DB_HOST', 'localhost');
+
+define('DB_CHARSET', 'utf8mb4');
+
+define('SECURE_AUTH_KEY', '8)fvMNnCKRq_2Zat&XSwD&a@/i|V5G~|lvoHoZ&xi^h<h/+`#624dlb6u+f]5OQ)');
+
+define('SECURE_AUTH_SALT', 'aAo0:L6?U,O+BjcU__tzy6H>;DdEXQzloPzgd2#"SekGjx+%;gx_Ikcv>#nlu~76');
+
+
+$table_prefix = 'wp_';
+
+require_once(ABSPATH . 'wp-settings.php');
